0aa91077c0828c92b2a976edaee68d8d6acea996
carlosilva.dev <cjrcordeiro@gmail.com>
2023-12-29 13:50:19 -0300

    Criado lógica para operações da classe CalculatorService

--- src/main/java/service/Calculator.java
+++ src/main/java/service/Calculator.java
@@ -2,4 +2,8 @@ package service;
 
 public interface Calculator {
     double sum(double valueOne, double valueTwo);
+    double divide(double valueOne, double valueTwo);
+    double subtract(double valueOne, double valueTwo);
+    double multiply(double valueOne, double valueTwo);
+    double rootSquare(double value);
 }
--- src/main/java/service/CalculatorService.java
+++ src/main/java/service/CalculatorService.java
@@ -4,6 +4,26 @@ public class CalculatorService implements Calculator {
 
     @Override
     public double sum(double valueOne, double valueTwo) {
-        return 0;
+        return valueOne + valueTwo;
+    }
+
+    @Override
+    public double divide(double valueOne, double valueTwo) {
+        return valueOne / valueTwo;
+    }
+
+    @Override
+    public double subtract(double valueOne, double valueTwo) {
+        return valueOne - valueTwo;
+    }
+
+    @Override
+    public double multiply(double valueOne, double valueTwo) {
+        return valueOne * valueTwo;
+    }
+
+    @Override
+    public double rootSquare(double value) {
+        return Math.sqrt(value);
     }
 }
